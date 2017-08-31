20e3fe860c6b9acc9770cb272e0e3c03f11c2d9d
thomas3d <thomas@etjam.dk>
2017-08-31 10:29:04 +0200

    Update MyLinqExtensions.cs

--- MyLinqExtensions.cs
+++ MyLinqExtensions.cs
@@ -1,12 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
-
-public class MyLinqExtensions
+namespace MyLinqExtensions
 {
+    public static class Extensions
+    {
+        public static T As<T>(this object input) where T : class
+        {
+            return input as T;
+        }        
+ 
+        public static bool In<T>(this T self, IEnumerable<T> input)
+        {
+            return input.Contains(self);
+        }
 
- public static bool In<T>(this T o, params T[] values) 
- {
-    return values.Contains(o);
- }
-
-
+        public static bool In<T>(this T self, params T[] input)
+        {
+            return input.Contains(self);
+        }              
+    }
 }
