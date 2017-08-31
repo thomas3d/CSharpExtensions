29492fc2e8c67b0e7bce1cc573315e7a3517b470
thomas3d <thomas@etjam.dk>
2017-08-31 10:36:30 +0200

    Add files via upload

---
+++ MyDateTimeExtentions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDateTimeExtensions
+{
+    public static class DateTimeExtentions
+    {
+        public static bool IsToday(this DateTime dateTime)
+        {
+            return (dateTime.Date == DateTime.Now.Date);
+        }
+    }
+}
