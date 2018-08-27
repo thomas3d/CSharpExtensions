c6fae0aa5f064147ca67da8846df2bb2a54a81b1
thomas3d <thomas@etjam.dk>
2018-08-27 10:24:12 +0200

    Update MyStringExtentions.cs

--- MyStringExtentions.cs
+++ MyStringExtentions.cs
@@ -8,6 +8,113 @@ namespace MyStringExtensions
 {
     public static class Extensions
     {
+        public static bool? TryToBool(this string str)
+		{
+			if (string.IsNullOrEmpty(str))
+			{
+				return null;
+			}
+
+			bool b;
+			if (bool.TryParse(str, out b))
+			{
+				return b;
+			}
+
+			return null;
+		}
+
+		public static bool ToBool(this string str)
+		{
+			if (string.IsNullOrEmpty(str))
+			{
+				return false;
+			}
+
+			bool b;
+			return bool.TryParse(str, out b) && b;
+		}
+
+		public static bool IsAny(this string str)
+		{
+			return !string.IsNullOrEmpty(str);
+		}
+
+		public static bool HasAny(this string str)
+		{
+			return IsAny(str);
+		}
+
+		public static bool HasNoChar(this string str)
+		{
+			return string.IsNullOrWhiteSpace(str);
+		}
+		public static bool HasChar(this string str)
+		{
+			return !HasNoChar(str);
+		}
+		public static bool IsNullOrEmpty(this string str)
+		{
+			return string.IsNullOrEmpty(str);
+		}
+
+		public static int ToInt(this string str)
+		{
+			int i;
+			return int.TryParse(str, out i) ? i : 0;
+		}
+
+		public static int TryToInt(this string str, int defaultValue)
+		{
+			int i;
+			return int.TryParse(str, out i) ? i : defaultValue;
+		}
+
+		public static string Restrict(this string str, int len)
+		{
+			if (string.IsNullOrEmpty(str))
+			{
+				return string.Empty;
+			}
+			if (len >= str.Length)
+			{
+				return str;
+			}
+			if (len <= 0)
+			{
+				return string.Empty;
+			}
+
+			return str.Substring(0, len);
+		}
+
+		public static bool DoContains(this string str, string input)
+		{
+			return str != null && str.Contains(input);
+		}
+
+		public static string TrimEnd(this string str, string item)
+		{
+			if (string.IsNullOrEmpty(str)) return str;
+			if (string.IsNullOrEmpty(item)) return str;
+			if (str.EndsWith(item)) return str.Substring(0, str.LastIndexOf(item));
+			return str;
+		}
+
+        /// <summary>
+        /// Returns string as a byte array using the specified encoding
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+	    public static byte[] ToByteArray(this string str, Encoding encoding)
+	    {
+	        return encoding.GetBytes(str);
+	    }
+
+		public static string[] Split(this string str, string separator) =>
+			str.Split(new[] { separator }, StringSplitOptions.None);
+
         public static string StringBetween(this string s, string before, string after)
         {
             if (!s.Contains(before) || !s.Contains(after))
